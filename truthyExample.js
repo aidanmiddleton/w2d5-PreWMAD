5b6330a6d496266ac6be1ae8e267797cf3add311
Aidan Middleton <aidanemiddleton@gmail.com>
2020-08-14 11:12:56 -0700

    added some more examples

---
+++ truthyExample.js
@@ -0,0 +1,8 @@
+let example1 = 10;
+let example2 = false;
+
+if (example1) {
+  console.log('it was true')
+} else {
+  console.log('it was false')
+}
---
+++ whileLoop.js
@@ -0,0 +1,5 @@
+let counter = 5
+
+while (counter < 15) {
+  console.log(counter);
+}
